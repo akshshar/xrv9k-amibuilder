a2fae2f19b58b31d7a9dc005ab83f499e24064fd
akshshar <akshat31187@gmail.com>
2021-04-30 03:46:49 +0530

    Fixing path to ssh keys

--- variables.tf
+++ variables.tf
@@ -7,12 +7,12 @@ variable "xrv9k_iso_name" {
 }
 
 variable "ssh_key_public" {
-  default     = "~/.ssh/id_rsa.pub"
+  default     = "./ssh/id_rsa.pub"
   description = "Path to the SSH public key for accessing cloud instances. Used for creating AWS keypair."
 }
 
 variable "ssh_key_private" {
-  default     = "~/.ssh/id_rsa"
+  default     = "./ssh/id_rsa"
   description = "Path to the SSH public key for accessing cloud instances. Used for creating AWS keypair."
 }
 
